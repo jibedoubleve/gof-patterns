f568e9e9988fe6af9fa2eaff28fa191a0f0e7e3f
jibedoubleve <jibedoubleve@f6c8e51f-85ae-cb7b-2285-0f49f97c2b0f>
2011-11-16 14:09:05 +0000

    Implemented Command

---
+++ trunk/src/Patterns/Behavioural/Command/ICommand.cs
@@ -0,0 +1,20 @@
+﻿namespace Design.Patterns.Behavioural.Command
+{
+    public interface ICommand
+    {
+        #region Properties
+
+        int Result
+        {
+            get;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        void Run();
+
+        #endregion Methods
+    }
+}
