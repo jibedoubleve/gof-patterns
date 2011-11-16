69cd720cd1a4f619835b8b12b375b653b7a27364
jibedoubleve <jibedoubleve@f6c8e51f-85ae-cb7b-2285-0f49f97c2b0f>
2011-11-16 12:43:10 +0000

    Implemented Flyweight

---
+++ trunk/src/Patterns/Structural/Flyweight/CachingFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace Design.Patterns.Structural.Flyweight
+{
+    using System.Collections.Generic;
+
+    public class CachingFactory
+    {
+        #region Fields
+
+        private static Dictionary<string, CachedUser> repository = new Dictionary<string, CachedUser>();
+
+        #endregion Fields
+
+        #region Methods
+
+        public static IUser GetUser(string id)
+        {
+            if (repository.ContainsKey(id))
+            {
+                repository[id].IsFromCache = true;
+                return repository[id];
+            }
+            else
+            {
+                var user = new CachedUser(id);
+                user.IsFromCache = false;
+                repository.Add(id, user);
+                return user;
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ trunk/src/Patterns/Structural/Flyweight/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Design.Patterns.Structural.Flyweight
+{
+    public class NonCachedUser : IUser
+    {
+        #region IUser Members
+        public NonCachedUser(string id)
+        {
+            this.Id = id;
+            this.IsFromCache = false;
+        }
+        public DateTime Birthdate
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string Id
+        {
+            get;
+            private set;
+        }
+
+        public bool IsFromCache
+        {
+            get;
+            private set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
