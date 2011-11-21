3fe5b74f209d2d8ccfb2e6f662ba088144b2eace
jibedoubleve <jibedoubleve@f6c8e51f-85ae-cb7b-2285-0f49f97c2b0f>
2011-11-21 12:14:34 +0000

    Implemented Strategy

---
+++ src/Patterns/Behavioural/Strategy/Speaker.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Design.Patterns.Behavioural.Strategy
+{
+    public class Speaker : ILanguage
+    {
+        private ILanguage language;
+        public Speaker(ILanguage language)
+        {
+            this.language = language;
+        }
+
+        #region ILanguage Members
+
+        public string SayHello()
+        {
+            return this.language.SayHello();
+        }
+
+        public string SayBye()
+        {
+            return this.language.SayBye();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Test/Behavioural/StrategyTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Design.Test.Behavioural
+{
+    using NUnit.Framework;
+    using Design.Patterns.Behavioural.Strategy;
+
+    [TestFixture]
+    public class StrategyTest
+    {
+        #region Methods
+
+        [Test]
+        public void CanUserStrategy()
+        {
+            var speaker = new Speaker(new French());
+
+            Assert.AreEqual("bonjour", speaker.SayHello().ToLower());
+            Assert.AreEqual("au revoir", speaker.SayBye().ToLower());
+
+            speaker = new Speaker(new English());
+
+            Assert.AreEqual("hello", speaker.SayHello().ToLower());
+            Assert.AreEqual("bye", speaker.SayBye().ToLower());
+        }
+
+        #endregion Methods
+    }
+}
